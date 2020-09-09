8630746f025135f56b01533f63673d018382d471
abhison990 <46558620+abhison990@users.noreply.github.com>
2020-09-09 13:47:17 +0200

    Add files via upload

---
+++ wcr.sh
@@ -0,0 +1,9 @@
+export spark_version=2.3
+USER_HOME=/home/airflow/wifi_uc
+
+spark2-submit --master yarn --deploy-mode cluster --files $USER_HOME/config_param.json \
+			  --driver-memory 4g --num-executors 3 --executor-cores 6 --executor-memory 12g \
+			  --keytab /home/airflow/keytab/tsystems_asondkar.keytab \
+		      --principal tsystems_asondkar@CDHDEV1.DC.HT.HR \
+             $USER_HOME/wificlientrole.py
+			 
